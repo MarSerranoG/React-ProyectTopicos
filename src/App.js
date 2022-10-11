743919d922efcd9a912e4c7ba62456c7093e078d
MarSerranoG <89483003+MarSerranoG@users.noreply.github.com>
2022-10-11 15:03:51 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ export default function App() {
   return (
     <div>
       <h1>Hello React!</h1>
+      <p> Mar :)</p>
+
       <Carro  
       marca="Camaro96" 
-      imagen = "https://s1.cdn.autoevolution.com/images/news/someone-has-recreated-the-chevrolet-camaro-from-gorillazs-stylo-for-gts-143187-7.jpg"/>
-      <p>Descripción del carro</p>
+      imagen = "https://s1.cdn.autoevolution.com/images/news/someone-has-recreated-the-chevrolet-camaro-from-gorillazs-stylo-for-gts-143187-7.jpg"
+      />
+      <p>1969 Chevy Camaro/Stylo</p>
+
       <Moto 
       marca = "Harley Davidson CVO Limited"
       imagen = "https://www.formulamoto.es/wp-content/uploads/2021/08/triumph_rocket_iii_tfc.jpg"
       />
-      <p>Descripción de la moto</p>
+      <p>Motor 2. Twin-Cooled™ Milwaukee-Eight™ 117.</p>
+
+
     </div>
   );
 }
